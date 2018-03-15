27223ed493be35db8952b338eaedb4153c6d3b6a
Gram <master_fess@mail.ru>
2018-03-15 12:43:01 +0500

    more test coverage

--- 1hello/main_test.go
+++ 1hello/main_test.go
@@ -5,8 +5,14 @@ import "testing"
 func TestHello(t *testing.T) {
 	got := Hello("")
 	want := "Hello, World!\n"
-
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
 	}
+
+	got = Hello("user")
+	want = "Hello, User!\n"
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+
 }
